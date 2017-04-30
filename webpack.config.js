aa3cc1bc273a5474b9ad9d277f23c18c12d16864
Cris Grego <crisrguezgres@gmail.com>
2017-04-30 20:47:13 +0200

    Configure the enviroment with React/Babel/Webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      }
+    ]
+  }
+}
